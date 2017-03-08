a1d2948480218a3bee76492577ffbee19590d25b
Sundaresh Iyer <Sundaresh2912@gmail.com>
2017-03-08 08:21:11 +0530

    Row Editing Complete

--- SpreadsheetWPF/MainWindow.xaml.vb
+++ SpreadsheetWPF/MainWindow.xaml.vb
@@ -72,47 +72,10 @@ Namespace gridData
         Dim collection As PresentData
         Dim obj, obj2 As userData
         Dim rowData(20) As String, headerSelected As String = ""
-        Dim lastCellAddedIndex As Short = 0
+        Dim lastCellAddedIndex As Short = 0, rowIndex As Short = 0, columnIndex As Short = 0
+        Dim rowEditIndex As Integer, colEditIndex As Integer = 0
+
         Dim copyActivated As Boolean = False, cutActivated As Boolean = False, pasteActivated As Boolean = False
-        Private Sub colSize(sender As Object, e As SizeChangedEventArgs)
-
-            pnl_dock.Width = win_main.ActualWidth
-            pnl_dock.Height = win_main.ActualHeight
-
-
-        End Sub
-
-        Private Sub btn_export_Click(sender As Object, e As RoutedEventArgs)
-
-        End Sub
-
-        Private Sub btn_import_Click(sender As Object, e As RoutedEventArgs)
-
-        End Sub
-
-        Private Sub btn_save_Click(sender As Object, e As RoutedEventArgs)
-
-        End Sub
-
-        Private Sub btn_validate_Click(sender As Object, e As RoutedEventArgs)
-
-        End Sub
-
-        Private Sub btn_close_Click(sender As Object, e As RoutedEventArgs)
-
-        End Sub
-
-        Private Sub columnHeader_PreviewMouseRightButtonUp(sender As Object, e As MouseButtonEventArgs) Handles dg_grid1.PreviewMouseRightButtonUp
-            If TypeOf sender Is DataGridColumn Then
-                Dim header As DataGridColumn = CType(sender, DataGridColumn)
-                headerSelected = header.Header.ToString
-            ElseIf TypeOf sender Is Primitives.DataGridColumnHeader Then
-                Dim header As Primitives.DataGridColumnHeader = CType(sender, Primitives.DataGridColumnHeader)
-                headerSelected = header.Content.ToString
-            End If
-            e.Handled = False
-
-        End Sub
 
         Public Sub New()
 
@@ -123,25 +86,133 @@ Namespace gridData
 
         End Sub
 
+        Private Sub colSize(sender As Object, e As SizeChangedEventArgs)
+
+            pnl_dock.Width = win_main.ActualWidth
+            pnl_dock.Height = win_main.ActualHeight
+
+
+        End Sub
+
+        Private Sub columnHeader_PreviewMouseRightButtonUp(sender As Object, e As MouseButtonEventArgs) Handles dg_grid1.PreviewMouseRightButtonUp
+
+            Dim dep As DependencyObject = e.OriginalSource
+
+            While (Not (dep Is Nothing) AndAlso Not (TypeOf dep Is DataGridCell) AndAlso Not (TypeOf dep Is Primitives.DataGridColumnHeader))
+                dep = VisualTreeHelper.GetParent(dep)
+            End While
+
+            If dep Is Nothing Then
+                Return
+            End If
+
+            If (TypeOf dep Is Primitives.DataGridColumnHeader) Then
+                Dim header As Primitives.DataGridColumnHeader = dep
+                headerSelected = header.Content.ToString
+            End If
+
+            If (TypeOf dep Is DataGridColumn) Then
+                Dim header As DataGridColumn = dep
+                headerSelected = header.Header.ToString
+            End If
+
+            If (TypeOf dep Is DataGridCell) Then
+                Dim cell As DataGridCell = dep
+                While (Not (dep Is Nothing) AndAlso Not (TypeOf dep Is DataGridRow))
+                    dep = VisualTreeHelper.GetParent(dep)
+
+                End While
+                Dim row As DataGridRow = dep
+
+                rowIndex = FindRowIndex(row)
+                columnIndex = cell.Column.DisplayIndex
+                MsgBox(rowIndex & " " & columnIndex)
+                ''If needed to find header of that row/column
+                'headerSelected = cell.Column.Header.ToString
+            End If
+            e.Handled = False
+            Return
+        End Sub
+
+        ''All the Helper Functions
+
+        Private Function FindRowIndex(row As DataGridRow) As Integer
+            Dim dataGrid As DataGrid = ItemsControl.ItemsControlFromItemContainer(row)
+            Dim index As Integer = dataGrid.ItemContainerGenerator.IndexFromContainer(row)
+            Return index
+        End Function
+
+
+        Private Function determineColumn(headerName As String, objectRef As userData) As Object
+            Select Case headerName
+                Case "Name"
+                    Return objectRef.name
+                Case "Selection"
+                    Return objectRef.selection
+                Case "Attribute1"
+                    Return objectRef.attribute1
+                Case "Attribute2"
+                    Return objectRef.attribute2
+                Case "Attribute3"
+                    Return objectRef.attribute3
+                Case "Attribute4"
+                    Return objectRef.attribute4
+                Case "Unit_Attri4"
+                    Return objectRef.unitattri4
+                Case "Attribute5"
+                    Return objectRef.attribute5
+                Case "Attribute6"
+                    Return objectRef.attribute6
+                Case "Attribute7"
+                    Return objectRef.attribute7
+                Case "Attribute8"
+                    Return objectRef.attribute8
+                Case "Attribute9"
+                    Return objectRef.attribute9
+                Case "Attribute10"
+                    Return objectRef.attribute10
+                Case "Attribute1"
+                    Return objectRef.attribute1
+                Case "MinimumVal"
+                    Return objectRef.minVal
+                Case "NormalVal"
+                    Return objectRef.normVal
+                Case "MaximumVal"
+                    Return objectRef.maxVal
+                Case Else
+                    MsgBox("Bug Column Name Determination : " + headerName)
+                    Return ""
+            End Select
+        End Function
+
+        ''End of Helper Functions
         Private Sub win_main_Initialized(sender As Object, e As EventArgs)
 
             collection = Me.Resources("presentData")
             collection.Clear()
-            obj = New userData("Name", "Selc", "1", "2", "3", "dd", "asd", "dd", "ad", "3", "3", "3", "3", 1, 2, 3)
+            obj = New userData("Name", "Selc", "1", "2", "3", "dd", "asd", "dd", "ad", "3", "20", "3", "3", 1, 2, 3)
             collection.Add(obj)
-            obj2 = New userData("Name", "Selc", "1", "2", "111", "dd", "asd", "dd", "ad", "3", "3", "3", "3", 1, 2, 3)
+            obj2 = New userData("Name", "Selc", "1", "2", "111", "dd", "asd", "dd", "ad", "435", "2", "3", "3", 1, 2, 3)
             collection.Add(obj2)
-            obj2 = New userData("Name", "Selc", "1", "2", "222", "dd", "asd", "dd", "ad", "3", "3", "3", "3", 1, 2, 3)
+            obj2 = New userData("Name", "Selc", "1", "2", "222", "dd", "asd", "dd", "ad", "22", "1", "3", "3", 1, 2, 3)
             collection.Add(obj2)
             ''dg_grid1.ItemsSource = collection
         End Sub
 
         Private Sub btn_edit_row_Click(sender As Object, e As RoutedEventArgs)
-            MsgBox(sender.GetType.ToString)
-            collection.Add(New userData())
+            If colEditIndex = 16 Then
+                collection.Insert(rowEditIndex + 1L, New userData())
+            End If
+
         End Sub
 
         Private Sub btn_delete_row_Click(sender As Object, e As RoutedEventArgs)
+            If colEditIndex = 17 Then
+                collection.RemoveAt(rowEditIndex)
+                If collection.Count = 0 Then
+                    collection.Add(New userData())
+                End If
+            End If
 
         End Sub
 
@@ -150,23 +221,17 @@ Namespace gridData
 
         End Sub
 
-        Private Sub cbCompleteFilter_Checked(sender As Object, e As RoutedEventArgs)
-
-        End Sub
-
-        Private Sub cbCompleteFilter_Unchecked(sender As Object, e As RoutedEventArgs)
-
-        End Sub
-
-
-        Private Sub PasteCommand_Executed(sender As Object, e As ExecutedRoutedEventArgs)
-
-        End Sub
-
-
         Private Sub menuFilter_Click(sender As Object, e As RoutedEventArgs)
             MsgBox(headerSelected)
         End Sub
+
+        'Private Sub cbCompleteFilter_Checked(sender As Object, e As RoutedEventArgs)
+
+        'End Sub
+
+        'Private Sub cbCompleteFilter_Unchecked(sender As Object, e As RoutedEventArgs)
+
+        'End Sub
 
         Private Sub CutCommand_Executed(sender As Object, e As ExecutedRoutedEventArgs)
             If Not cutActivated Then
@@ -214,50 +279,62 @@ Namespace gridData
 
         End Sub
 
-        Private Function determineColumn(headerName As String, objectRef As userData) As Object
-            Select Case headerName
-                Case "Name"
-                    Return objectRef.name
-                Case "Selection"
-                    Return objectRef.selection
-                Case "Attribute1"
-                    Return objectRef.attribute1
-                Case "Attribute2"
-                    Return objectRef.attribute2
-                Case "Attribute3"
-                    Return objectRef.attribute3
-                Case "Attribute4"
-                    Return objectRef.attribute4
-                Case "Unit_Attri4"
-                    Return objectRef.unitattri4
-                Case "Attribute5"
-                    Return objectRef.attribute5
-                Case "Attribute6"
-                    Return objectRef.attribute6
-                Case "Attribute7"
-                    Return objectRef.attribute7
-                Case "Attribute8"
-                    Return objectRef.attribute8
-                Case "Attribute9"
-                    Return objectRef.attribute9
-                Case "Attribute10"
-                    Return objectRef.attribute10
-                Case "Attribute1"
-                    Return objectRef.attribute1
-                Case "MinimumVal"
-                    Return objectRef.minVal
-                Case "NormalVal"
-                    Return objectRef.normVal
-                Case "MaximumVal"
-                    Return objectRef.maxVal
-                Case Else
-                    MsgBox("Bug Column Name Determination : " + headerName)
-                    Return ""
-            End Select
-        End Function
+
+        Private Sub PasteCommand_Executed(sender As Object, e As ExecutedRoutedEventArgs)
+
+        End Sub
+
+        Private Sub detectCellClicked(sender As Object, e As MouseButtonEventArgs) Handles dg_grid1.PreviewMouseLeftButtonUp
+
+            Dim dep As DependencyObject = e.OriginalSource
+
+            While (Not (dep Is Nothing) AndAlso Not (TypeOf dep Is DataGridCell) AndAlso Not (TypeOf dep Is Primitives.DataGridColumnHeader))
+                dep = VisualTreeHelper.GetParent(dep)
+            End While
+
+            If dep Is Nothing Then
+                Return
+            End If
+
+            If (TypeOf dep Is DataGridCell) Then
+                Dim cell As DataGridCell = dep
+                While (Not (dep Is Nothing) AndAlso Not (TypeOf dep Is DataGridRow))
+                    dep = VisualTreeHelper.GetParent(dep)
+                End While
+                Dim row As DataGridRow = dep
+
+                rowEditIndex = FindRowIndex(row)
+                colEditIndex = cell.Column.DisplayIndex
+            End If
+            e.Handled = False
+            Return
+
+        End Sub
 
         Private Sub dg_grid1_LoadingRow(sender As Object, e As DataGridRowEventArgs)
 
         End Sub
+
+        ''Yet to be Impleted Codes
+        Private Sub btn_export_Click(sender As Object, e As RoutedEventArgs)
+
+        End Sub
+
+        Private Sub btn_import_Click(sender As Object, e As RoutedEventArgs)
+
+        End Sub
+
+        Private Sub btn_save_Click(sender As Object, e As RoutedEventArgs)
+
+        End Sub
+
+        Private Sub btn_validate_Click(sender As Object, e As RoutedEventArgs)
+
+        End Sub
+
+        Private Sub btn_close_Click(sender As Object, e As RoutedEventArgs)
+
+        End Sub
+
     End Class
 End Namespace
