fc51dfbac556894e5f5fdea822ee88969831bd31
sayanhere <sayan.roy@infosys.com>
2017-04-04 17:01:12 +0530

    Constructor Parameter changed

--- SpreadsheetWPF/MainWindow.xaml.vb
+++ SpreadsheetWPF/MainWindow.xaml.vb
@@ -12,6 +12,8 @@ Imports System.Collections.Specialized
 Imports SPF.Client.Administration
 Imports SPF.Client.Schema.Collection
 Imports SPF.Client.Schema.Interface
+'Imports SPF.Client.APIs
+Imports SPF.Client
 'Imports SPF.Client.api.wpf
 'This is comment
 
@@ -162,6 +164,7 @@ Namespace gridData
 
     Class MainWindow
         Dim spfsessioninfo As SPFSession = Nothing
+        Dim formclient As SPFWindowsFormsClient = Nothing
         Dim collection As PresentData
         Dim headerList() As String = {"Item Tag Name", "Measuring Principle", "Measuring/Adjust Location", "PID Sheet Number", "Construction Status", "Pressure P1 Minimum", "Pressure P1 In Operation", "Pressure P1 Maximum", "Unit Of Pressure P1", "Temperature Minimum", "Temperature In Operation", "Temperature Maximum", "Unit Of Temperature", "Differential Pressure Minimum", "Differential Pressure In Operation", "Differential Pressure Maximum", "Unit of Differential Pressure"}
 
@@ -408,8 +411,9 @@ Namespace gridData
                 defaultData_dgGrid()
             End If
         End Sub
-        Public Sub New(list() As userData, Optional Spfsessionid As SPFSession = Nothing)
+        Public Sub New(list() As userData, Optional Spfsessionid As SPFSession = Nothing, Optional spfwindowsfrmClient As SPFWindowsFormsClient = Nothing)
             spfsessioninfo = CType(Spfsessionid, SPFSession)
+            formclient = CType(spfwindowsfrmClient, SPFWindowsFormsClient)
             ' This call is required by the designer.
             InitializeComponent()
             collection = Me.Resources("presentData")
